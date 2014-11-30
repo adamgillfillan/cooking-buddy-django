c67314eb7b019188abaac07f3edd4ab8ca06b800
Brandon Godwin <brandon@brandongodwin.net>
2014-11-30 01:44:39 -0500

    Added survey links to navbar. Links will automatically fill in the user's name.

--- CSC591/settings.py
+++ CSC591/settings.py
@@ -90,3 +90,8 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+)
